766fcc85fa6c1144f459cd1b1fffe26f1c3ef757
Abril Abonizio <abril.abonizio@gmail.com>
2026-06-03 11:44:12 -0300

    Método de los Cuadrados
    Se divide en modulos y se implementa el generador CM

--- numerosPseudoaleatorios2-1.py
+++ numerosPseudoaleatorios2-1.py
@@ -13,32 +13,85 @@ longitud_sucesion = int(sys.argv[4])
 
 sucesion = []
 seed = 0
-mGCL = pow(2,32)
 
-while seed <= 0 or seed >= mGCL:
-    seed = int(input("Ingresar la semilla: "))
-    if seed <= 0 or seed >= mGCL:
-        print("La semilla debe ser un valor entero entre 0 y 4.294.967.296")
-sucesion.append(seed)
+def generador_GCL(seed):
+  
+  mGCL = pow(2,32) 
+  while seed <= 0 or seed >= mGCL:
+      seed = int(input("Ingresar la semilla: "))
+      if seed <= 0 or seed >= mGCL:
+          print("La semilla debe ser un valor entero entre 0 y 4.294.967.296")
+  sucesion.append(seed)
+
+  if tipo_generador == "GCL":
+    a = random.randint(0,mGCL-1 // 4) * 4 + 1  
+    c = random.randint(0,mGCL-1)
+    c = c | 1 # Fuerza que sea impar
+    for i in range(0,longitud_sucesion):
+        sucesion.append((a*sucesion[-1]+c) % mGCL)
+
+
+  print("a utilizado = " + str(a))
+  print("c utilizado = " + str(c))
+  print("m utilizado = " + str(mGCL))
+  print("Los " + str(longitud_sucesion) + " generados son: ")
+  print(sucesion[1:])
+  
+  return seed
+
+valores = []
+
+def generador_CM():  # CM = Cuadrados Medios
+   
+  seed = ""
+    
+  while not seed.isdigit() or int(seed) <= 0 or len(seed) != 4:
+    seed = input("Ingresar la semilla: ")
+    if not seed.isdigit() or int(seed) <= 0 or len(seed) != 4:
+      print("La semilla debe ser un valor entero mayor a 0 y de 4 dígitos")
+  
+    
+  seed_entero = int(seed)
+  sucesion.append(seed_entero)
+  nueva_semilla = seed_entero
+
+  for _ in range(0, longitud_sucesion):
+    
+    valor = nueva_semilla ** 2
+    valor_str = str(valor)
+        
+    if len(valor_str) != 8:
+      valor_str = valor_str.zfill(8)
+
+    valores.append(int(valor_str))    
+    centro = valor_str[2:6]
+    nueva_semilla = int(centro)
+
+    sucesion.append(nueva_semilla)
+
+  
+  print("Valores: ", valores)
+  
+  print("Los " + str(longitud_sucesion) + " generados son: ")
+  print(sucesion[1:])
+  return seed 
+
+
 
 if tipo_generador == "GCL":
-   a = random.randint(0,mGCL-1 // 4) * 4 + 1  
-   c = random.randint(0,mGCL-1)
-   c = c | 1 # Fuerza que sea impar
-   for i in range(0,longitud_sucesion):
-      sucesion.append((a*sucesion[-1]+c) % mGCL)
+  seed = generador_GCL(seed) 
+elif tipo_generador == "CM":
+  seed = generador_CM()
 
-print("a utilizado = " + str(a))
-print("c utilizado = " + str(c))
-print("m utilizado = " + str(mGCL))
-print("Los " + str(longitud_sucesion) + " generados son: ")
-print(sucesion[1:])
+
+
+
 
 plt.figure(figsize=(10,5))
 plt.suptitle(f"{longitud_sucesion} números generados - Generador {tipo_generador} - Semilla {seed}")
 plt.title("Números generados")
 plt.xlabel("n")
-plt.ylabel("res (Nómero generado)")
+plt.ylabel("res (Número generado)")
 generacion = list(range(1, longitud_sucesion + 1))
 plt.scatter(generacion, sucesion[1:], color='red', s=10, label='Número generado')
 
