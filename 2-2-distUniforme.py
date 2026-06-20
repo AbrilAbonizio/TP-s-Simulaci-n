0ddc88f8557cea7954ed084dbfde3c68cf3c33f3
Abril Abonizio <abril.abonizio@gmail.com>
2026-06-19 21:30:11 -0300

    Correcciones

--- 2-2-distUniforme.py
+++ 2-2-distUniforme.py
@@ -71,59 +71,48 @@ plt.show()
 # TEST GENERAL
 # ==========================================
 
-def test_general():
+def test_general(eTeorica, vTeorica):
 
-    esperanza_teorica = (a + b) / 2
-    varianza_teorica = (b - a) ** 2 / 12
+    sumaMedia = 0
+    for n in sucesion:
+        sumaMedia += n
+    esperanza = sumaMedia / longitud_sucesion
 
-    esperanza = np.mean(sucesion)
-    varianza = np.var(sucesion, ddof=1)
+    dif_abs_e = abs(esperanza - eTeorica)
 
     print("\n========== TEST GENERAL ==========")
-
-    print(f"\nEsperanza teórica : {esperanza_teorica:.4f}")
-    print(f"Esperanza obtenida: {esperanza:.4f}")
-
-    print(f"\nVarianza teórica : {varianza_teorica:.4f}")
-    print(f"Varianza obtenida: {varianza:.4f}")
-
-# ==========================================
-# CHI CUADRADO
-# ==========================================
-
-def test_chi_cuadrado(alfa=0.05):
-
-    print("\n========== CHI-CUADRADO ==========")
-
-    k = 10
-
-    intervalos = np.linspace(a, b, k + 1)
-
-    observados, _ = np.histogram(
-        sucesion,
-        bins=intervalos
-    )
-
-    esperados = np.ones(k) * len(sucesion) / k
-
-    chi2 = np.sum(
-        (observados - esperados) ** 2 / esperados
-    )
-
-    grados_libertad = k - 1
-
-    p_valor = 1 - stats.chi2.cdf(
-        chi2,
-        grados_libertad
-    )
-
-    print(f"Chi² = {chi2:.4f}")
-    print(f"p-valor = {p_valor:.6f}")
-
-    if p_valor < alfa:
-        print("SE RECHAZA H0")
+    print("ESPERANZA:")
+    print("Teorica: ", eTeorica)
+    print("Obtenida: ", esperanza)
+    print("Diferencia absoluta: ", dif_abs_e)
+    margen_permitido_e = 1.96 * math.sqrt(vTeorica) / math.sqrt(longitud_sucesion)
+    print("Margen permitido: ", margen_permitido_e)
+    print("Veredicto:")
+    if dif_abs_e <= margen_permitido_e:
+        print("LA ESPERANZA PASA EL TEST")
     else:
-        print("NO SE RECHAZA H0")
+        print("LA ESPERANZA NO PASA EL TEST")
+
+    sumaVarianza = 0
+    for n in sucesion:
+        sumaVarianza = sumaVarianza + (n - esperanza) ** 2
+    varianza = sumaVarianza / (longitud_sucesion - 1)
+
+    errorRelV = abs((varianza - vTeorica) / vTeorica)
+
+    print("VARIANZA:")
+    print("Teorica: ", vTeorica)
+    print("Obtenida: ", varianza)
+    print("Error relativo: ", errorRelV)
+    margen_permitido_v = 0.05
+    print("Margen permitido: ", margen_permitido_v)
+    print("Veredicto:")
+    if errorRelV <= margen_permitido_v:
+        print("LA VARIANZA PASA EL TEST")
+    else:
+        print("LA VARIANZA NO PASA EL TEST")
+
+
 
 # ==========================================
 # KOLMOGOROV-SMIRNOV
@@ -151,8 +140,10 @@ def test_kolmogorov(alfa=0.05):
 # EJECUCIÓN
 # ==========================================
 
-test_general()
+eTeorica = (a + b) / 2
+vTeorica = (b - a) ** 2 / 12
 
-test_chi_cuadrado()
+test_general(eTeorica, vTeorica)
+
 
 test_kolmogorov()
